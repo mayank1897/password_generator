dbc27ec85135247cb84f8ebbe4ff5a27a69ab881
mayank1897 <62796466+mayank1897@users.noreply.github.com>
2020-05-25 21:57:00 +0530

    first commit

---
+++ password_generator.py
@@ -0,0 +1,30 @@
+import string
+import random
+
+
+def pass_gen(pass_size):
+    s1=list(string.ascii_lowercase)
+    s2=list(string.ascii_uppercase)
+    s3=list(string.digits)
+    s4=list(string.punctuation)
+
+    c=[]
+    c.extend(s1)
+    c.extend(s2)
+    c.extend(s3)
+    c.extend(s4)
+    random.shuffle(c)
+    return ("".join(c[:pass_size]))
+   
+
+if __name__ == "__main__":
+    while(True):
+        n=input("Enter the size of your password= ")
+        if (n.isdigit()==True):
+            n=int(n)
+            break
+        else:
+            print("input is not as integer")
+    print(f"Generated Password= {pass_gen(n)}")
+
+
